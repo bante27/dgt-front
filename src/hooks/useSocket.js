3452f1148cc094aee94a9350ba749757499ec36e
bante27 <mitikubantalem@gmail.com>
2026-08-23 23:49:05 -0900

    update socket

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:5000'; // Replace with your backend URL
+const SOCKET_URL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL.replace('/api', '') : 'http://localhost:5000';
 
 export const useSocket = (token) => {
   const [socket, setSocket] = useState(null);
